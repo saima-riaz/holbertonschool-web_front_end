8885ffae14b2ef90098c8a40305b8a5b7bc61ed3
saima-riaz <saimariaz1422@gmail.com>
2024-09-06 19:25:33 +0200

    Log to the console the time in milliseconds to execute the function 100 times

---
+++ Javascript_advanced/10-prime.js
@@ -0,0 +1,18 @@
+function countPrimeNumbers() {
+    // Precomputed list of prime numbers between 2 and 100
+    let primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
+        31, 37, 41, 43, 47, 53, 59, 61, 67,
+        71, 73, 79, 83, 89, 97];
+
+    return primes.length;
+}
+
+let startTime = performance.now();
+
+for (let i = 0; i < 100000; i++) {
+    countPrimeNumbers();
+}
+
+let endTime = performance.now();
+
+console.log(`Execution time of calculating prime numbers 100 times was' ${endTime - startTime} milliseconds.`);
